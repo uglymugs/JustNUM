25b45096a44e4afac76e327640d0684cb182463b
Nogaenbar <noga.inbar@gmail.com>
2016-09-20 16:01:40 +0100

    add checkbox and text field components

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,7 @@ const loadStories = () => {
   require('../stories/index');
   require('../stories/icon_menu');
   require('../stories/case_checkboxes');
+  require('../stories/case_text_field');
 };
 
 configure(loadStories, module);
