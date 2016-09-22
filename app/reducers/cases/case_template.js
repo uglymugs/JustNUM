19b1681a88e09c834d20d5667eee91163ddb2916
Matt Glover <matthewglover@me.com>
2016-09-22 12:51:27 +0100

    tidy case reducer

---
+++ app/reducers/cases/case_template.js
@@ -0,0 +1,21 @@
+const CASE_TEMPLATE = {
+  app: false,
+  area: null,
+  contacts: null,
+  db: false,
+  email: false,
+  caseId: null,
+  info: false,
+  nc: false,
+  notesCreate: null,
+  operation: null,
+  pc: false,
+  police: false,
+  referral: null,
+  sp: false,
+  sms: false,
+  timeline: null,
+  web: false,
+};
+
+export default CASE_TEMPLATE;
