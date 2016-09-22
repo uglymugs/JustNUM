b16886c8db02d7e52604949d82f55822a13b8cd1
Matt Glover <matthewglover@me.com>
2016-09-22 11:57:13 +0100

    add ADD_CASE to cases reducer

--- app/action_types/index.js
+++ app/action_types/index.js
@@ -1,4 +1,2 @@
-export const ADD_TODO = 'ADD_TODO';
-export const REMOVE_TODO = 'REMOVE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const ADD_CASE = 'ADD_CASE';
+export const EDIT_CASE = 'EDIT_CASE';
