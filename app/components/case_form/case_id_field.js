1f281c5a373ef2afcc7d7382845c7c5626a08446
heatherlauren <heatherlaurendavidson@gmail.com>
2016-09-22 13:44:18 +0100

    styling fixes for case form

--- app/components/case_form/case_id_field.js
+++ app/components/case_form/case_id_field.js
@@ -5,7 +5,7 @@ import TextField from '../material_ui_form_lib/text_field';
 const CaseIdFields = () =>
   <div className="CaseIdFields">
     <div>
-      <Field name="CaseIdFields__caseId" component={TextField} label="Case Id" />
+      <Field name="CaseIdFields__caseId" component={TextField} label="Case ID" />
     </div>
   </div>;
 
