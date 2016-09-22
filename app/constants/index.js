f555b997e4b39751d8b960633fa59b5e579adf72
mcelearr <rorymcelearney@gmail.com>
2016-09-22 12:36:09 +0100

    remove todo references and update package json

--- app/constants/index.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE',
-};
