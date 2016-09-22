4c26fe12814f2a967fad1cc1da98383be854bb8c
heatherlauren <heatherlaurendavidson@gmail.com>
2016-09-22 16:30:44 +0100

    add homepage, reroute homepage link in sidebar

---
+++ app/components/homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FakeHome = () =>
+  <div>
+    <img className="HomepageImage" src="app/assets/homepage.png" role="presentation" />
+  </div>;
+
+
+export default FakeHome;
