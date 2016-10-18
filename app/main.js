fefdd773dbaf85762edbe705798ef7368ed83496
samgalson@gmail.com <samgalson@gmail.com>
2016-10-18 14:29:46 +0100

    configure proxy for deployd

--- app/main.js
+++ app/main.js
@@ -22,7 +22,6 @@ render(
   document.getElementById('app')
 );
 
-/* is this necessary?
 // See https://github.com/gaearon/redux-devtools/tree/master/examples/todomvc
 if (module.hot) {
   module.hot.accept('./components/root', () => {
@@ -37,4 +36,4 @@ if (module.hot) {
     );
   });
 }
-*/
+
