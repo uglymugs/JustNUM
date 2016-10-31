140be8a13c6030a8952353ec449000c3ebaf523e
samgalson@gmail.com <samgalson@gmail.com>
2016-10-31 11:35:21 +0000

    add user to app bar

--- app/containers/logged_in_menu.js
+++ app/containers/logged_in_menu.js
@@ -1,25 +1,37 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ExitToApp from 'material-ui/svg-icons/action/exit-to-app';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import { logout } from '../action_creators';
+import { getFirstName, getLastName } from '../reducers';
 
 // eslint-disable-next-line no-shadow
-const SimpleLoggedInMenu = ({ logout }) =>
-  <IconMenu
-    iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-  >
-    <MenuItem primaryText="Sign out" onTouchTap={logout} />
-  </IconMenu>;
+const SimpleLoggedInMenu = ({ name, logout }) =>
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div style={{ color: 'grey' }}>
+      { `Signed in as ${name}`}
+    </div>
+    <IconMenu
+      iconButtonElement={<IconButton><ExitToApp /></IconButton>}
+      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      iconStyle={{ color: 'grey' }}
+    >
+      <MenuItem primaryText="Sign out" onTouchTap={logout} />
+    </IconMenu>
+  </div>;
 
 SimpleLoggedInMenu.propTypes = {
   logout: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
-const LoggedInMenu = connect(null, { logout })(SimpleLoggedInMenu);
+const mapStateToProps = (state) => ({
+  name: `${getFirstName(state)} ${getLastName(state)}`,
+});
+
+const LoggedInMenu = connect(mapStateToProps, { logout })(SimpleLoggedInMenu);
 
 export default LoggedInMenu;
