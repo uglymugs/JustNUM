98427745e3de620c550262b2a976bca04601996b
samgalson@gmail.com <samgalson@gmail.com>
2016-11-02 08:42:44 +0000

    add missing validation

--- app/form_validators/case.js
+++ app/form_validators/case.js
@@ -1,8 +1,23 @@
 import createValidate from '../lib/create_validate';
 
 const validate = createValidate({
-  caseId: (caseId) => {
-    if (!caseId) return 'Case id required';
+  caseRef: (caseRef) => {
+    if (!caseRef) return 'Case ID required';
+    return undefined;
+  },
+
+  referral: (referral) => {
+    if (!referral) return 'Referral required';
+    return undefined;
+  },
+
+  area: (area) => {
+    if (!area) return 'Area required';
+    return undefined;
+  },
+
+  operation: (operation) => {
+    if (!operation) return 'Operation required';
     return undefined;
   },
 
---
+++ app/lib/trace.js
@@ -0,0 +1,6 @@
+const t = m => v => {
+  console.log(m, v);
+  return v;
+};
+
+export default t;
