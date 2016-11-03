ea2925ebcca67c478161e340d4a5ceb4e48cc921
Matt Glover <matthewglover@me.com>
2016-11-03 11:44:24 +0000

    add case validation

--- app/form_validators/case.js
+++ app/form_validators/case.js
@@ -4,21 +4,7 @@ import * as api from '../api';
 const validate = createValidate({
   caseRef: (caseRef) => {
     if (!caseRef) return 'Case ID required';
-    return undefined;
-  },
-
-  referral: (referral) => {
-    if (!referral) return 'Referral required';
-    return undefined;
-  },
-
-  area: (area) => {
-    if (!area) return 'Area required';
-    return undefined;
-  },
-
-  operation: (operation) => {
-    if (!operation) return 'Operation required';
+    if (/\//.test(caseRef)) return 'Case Id cannot contain "/"';
     return undefined;
   },
 
